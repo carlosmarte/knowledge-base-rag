758997b6327dd97a984a217991d6b1ce37d05ea4
Carlos Marte <3478576+carlosmarte@users.noreply.github.com>
2025-07-01 11:30:58 -0400

    Update zsh_alias.sh

--- CLI/zsh_alias.sh
+++ CLI/zsh_alias.sh
@@ -1,1 +1,3 @@
-alias nuke_history='fc -R; fc -p; history -c; print -s "$(fc -ln -1)"; clear'
+alias zsh_clear_history='fc -R; fc -p; history -c; print -s "$(fc -ln -1)"; clear'
+
+alias zsh_del_history="history -c && rm -f ~/.zsh_history && touch ~/.zsh_history && fc -R && printf '\033c'"
