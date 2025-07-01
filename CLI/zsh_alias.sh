058a7e8b48ffc67f33e15dd79559804ba6c687b6
Carlos Marte <3478576+carlosmarte@users.noreply.github.com>
2025-07-01 11:34:06 -0400

    Update zsh_alias.sh

--- CLI/zsh_alias.sh
+++ CLI/zsh_alias.sh
@@ -1,3 +1,3 @@
-alias zsh_clear_history='fc -R; fc -p; history -c; print -s "$(fc -ln -1)"; clear'
+alias clear_history='fc -R; fc -p; history -c; print -s "$(fc -ln -1)"; clear'
 
-alias zsh_del_history="history -c && rm -f ~/.zsh_history && touch ~/.zsh_history && fc -R && printf '\033c'"
+alias zsh_del_history='fc -p; rm -f ~/.zsh_history; touch ~/.zsh_history; fc -R; clear; printf "\033c"'
